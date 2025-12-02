1b7b72fc054b2881adb3019821970487a713e769
Jesper Haug Karsrud <we-3PSopraJHK@autosync.no>
2025-12-02 15:41:08 +0100

    feature: Add schemas for recipes
    Adds schemas for a Recipe with ingredient objects. We don't need to store them
    as references, as they will change with every recipe.

--- packages/studio/schemaTypes/index.ts
+++ packages/studio/schemaTypes/index.ts
@@ -1,1 +1,91 @@
-export const schemaTypes = []
+import { defineField, defineType } from 'sanity';
+
+const IngredientUnits: Record<string, string> = {
+  ml: 'ml',
+  cl: 'cl',
+  dl: 'dl',
+  l: 'l',
+  mg: 'mg',
+  g: 'g',
+  kg: 'kg',
+};
+
+const Ingredient = defineType({
+  type: 'object',
+  title: 'Ingredient',
+  name: 'ingredient',
+  fields: [
+    defineField({
+      type: 'string',
+      title: 'Name',
+      name: 'name',
+    }),
+    defineField({
+      type: 'number',
+      title: 'Measure',
+      name: 'measure',
+    }),
+    defineField({
+      type: 'string',
+      title: 'Unit',
+      name: 'unit',
+      options: {
+        list: Object.entries(IngredientUnits).map(([key, value]) => ({
+          title: key,
+          value: value,
+        })),
+      },
+    }),
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      measure: 'measure',
+      unit: 'unit',
+    },
+    prepare: ({ title, measure, unit }) => ({
+      title: `${title} - ${measure} ${IngredientUnits[unit]}`,
+    }),
+  },
+});
+
+const Recipe = defineType({
+  title: 'Recipe',
+  name: 'recipe',
+  type: 'document',
+  fields: [
+    defineField({
+      title: 'Title',
+      name: 'title',
+      type: 'string',
+    }),
+    defineField({
+      title: 'Slug',
+      name: 'slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+      },
+    }),
+    defineField({
+      title: 'Ingredients',
+      name: 'ingredients',
+      type: 'array',
+      of: [{ type: 'ingredient' }],
+    }),
+    defineField({
+      title: 'Introduction',
+      name: 'introduction',
+      type: 'array',
+      of: [{ type: 'block' }],
+    }),
+    defineField({
+      title: 'Instructions',
+      name: 'instructions',
+      type: 'array',
+      of: [{ type: 'block' }],
+    }),
+  ],
+});
+
+export const schemaTypes = [Recipe, Ingredient];
